9218745418ea9ad2691078089f85bfc7131ad518
Lessflrs <126152753+Lessflrs@users.noreply.github.com>
2023-05-30 17:56:09 -0600

    Update number.sh

--- Scripting en Bash/number.sh
+++ Scripting en Bash/number.sh
@@ -1,1 +1,13 @@
-
+#LESSLIE MONTSERRAT MEDELLIN FLORES 
+#1960113
+#
+#!/bin/bash
+#Leer 3 numeros asisgnados a 3 variables
+read -p "Proporciona un numero para variable 1: " n1
+read -p "Proporciona un numero para variable 2: " n2
+read -p "Proporciona un numero para variable 3: " n3
+#
+#Despliega los 3 numeros proporcionados por el usuario
+echo "Numero 1 - $n1"
+echo "Numero 2 - $n2"
+echo "Numero 3 - $n3"
