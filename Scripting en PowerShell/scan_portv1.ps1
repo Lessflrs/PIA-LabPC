3f59759c38c3889913ab0610c13f867779e0e382
Lessflrs <126152753+Lessflrs@users.noreply.github.com>
2023-05-30 15:22:03 -0600

    Update scan_portv1.ps1

--- Scripting en PowerShell/scan_portv1.ps1
+++ Scripting en PowerShell/scan_portv1.ps1
@@ -1,1 +1,19 @@
+#LESSLIE MONTSERRAT MEDELLIN  FLORES 
+#1960113  
 
+$portstoscan = @(20,21,22,23,25,50,51,53,80,110,119,135,136,137,138,139,143,161,162,389,443,445,636,1025,1443,3389,5985,8080,10000)
+$waittime = 100
+
+Write-Host "Direccion ip a escanear: " -NoNewline
+$direccion = Read-Host 
+
+foreach ( $p in $portstoscan )
+{
+    $TCPObject = new-object System.Net.Sockets.TcpClient 
+        try{ $resultado = $TCPObject.ConnectAsync($direccion,$p).Wait($waittime)}catch{}
+        if ( $resultado -eq "True")
+        {
+            Write-Host "Puerto abierto: " -NoNewline; Write-Host $p -ForegroundColor Green 
+        }
+}
+Get-Date
