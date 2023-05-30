10437f4fede0b2c42f52231792aca2d762a8472b
Lessflrs <126152753+Lessflrs@users.noreply.github.com>
2023-05-30 15:36:00 -0600

    Update scrap1.py

--- Webscraping/scrap1.py
+++ Webscraping/scrap1.py
@@ -1,1 +1,10 @@
+#LESSLIE MONTSERRAT MEDELLIN FLORES 
+#1950113
 
+#Importar modulos
+import requests
+#Obtener la informacion HTMl de la URL
+URL = "https://realpython.github.io/fake-jobs/"
+page = requests.get(URL)
+#Imprimir el texto de la peticion GET
+print(page.text)
