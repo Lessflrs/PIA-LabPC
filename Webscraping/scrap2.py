779f647eea5c464ee7fce23b00c6d0cc405cb203
Lessflrs <126152753+Lessflrs@users.noreply.github.com>
2023-05-30 15:38:12 -0600

    Update scrap2.py

--- Webscraping/scrap2.py
+++ Webscraping/scrap2.py
@@ -1,1 +1,14 @@
+#LESSLIE MONTSERRAT MEDELLIN FLORES 
+#1960113
 
+#Importacion de modulos
+import requests
+from bs4 import BeautifulSoup 
+#Obtencion de los datos mediante peticion GET 
+URL = "https://realpython.github.io/fake-jobs/"
+page = requests.get(URL)
+#Analizamos el contenido HTML con BeautifulSoup
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id="ResultsContainer")
+#Formateamos la salida del objeto results de BeautifulSoup
+print(results.prettify())
