cfca31e0e075214d4ff1312f0e124085a596f248
Lessflrs <126152753+Lessflrs@users.noreply.github.com>
2023-05-30 18:12:22 -0600

    Update scan_portv2.py

--- Escaner de puertos/scan_portv2.py
+++ Escaner de puertos/scan_portv2.py
@@ -1,1 +1,20 @@
+#LESSLIE MONTSERRAT MEDELLIN FLORES 1960113
+#1960113
 
+import socket
+def scan(addr, port):
+    socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(1)
+    result = socket_obj.connect_ex((addr, port))
+    socket_obj.close()
+    return result
+ports=[21, 22, 25, 80]
+for i in range(1,255):
+    addr="192.168.0.{}".format(i)
+    for port in ports:
+        result=scan(addr, port)
+        if result==0:
+            print(addr, port, "OK")
+        else:
+            print(addr, port, "Failed")
+            
